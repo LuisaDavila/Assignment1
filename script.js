6ab03c86eb05b800c313586adc1bd2f2b19f0e72
Luisa <davilalu28@gmail.com>
2016-06-20 01:04:31 -0700

    Add existing file

---
+++ script.js
@@ -0,0 +1,167 @@
+    //STEP 1
+    //var someMonth
+    //function theMonth
+    //currentMonth
+    //var summerMonth
+    //myLibraryaFunction
+
+    //STEP 2 
+
+    4.2 //numeric literal
+    "Goodbye" //string literal 
+    true  //Boolean literal
+    null //literal null value
+
+    //STEP 3 
+
+    var someMath = 2 * (4 + 6);
+    var aExpression = 1 + 2;
+
+    //STEP 4
+
+    var firstName;
+    var lastName;
+    var address;
+    var city;
+    var state;
+    var zip;
+    var code;
+    var yourAge; 
+    var referralSource;
+    var mayWeContactYou;    
+
+
+    //STEP 5 
+    var firstName = "Luisa";
+    var lastName = "Davila";
+    var address = "1234 Street";
+    var city = "San Diego";
+    var state = "California";
+    var zip = "92104";
+    var code = "123";
+    var yourAge = "34";
+    var referralSource = "website";
+    var mayWeContactYou ="no";
+
+
+    //STEP 6
+
+    var numWeekDays = '5';
+    var numWeekendDays = 2; 
+    var totDays = + numWeekDays + numWeekendDays;
+    console.log(totDays);
+
+    var myName = "Luisa";
+    var isStudent = true;
+    console.log(isStudent);
+
+
+    var x = 20;
+    var y = 10;
+    console.log (x == y);
+
+    //STEP 7
+    
+
+    //STEP 8 
+
+   //No it is not valid.  
+   var someString = 'Who once said, Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.';
+   console.log(someString);
+    
+    // STEP 9
+    var a = 10;
+    var b = null;
+    console.log(a);
+    console.log(b);
+
+    // STEP 10
+    typeof "Luisa"
+    console.log("Luisa");
+
+    typeof 4
+    console.log(4);
+
+    typeof true
+    console.log(true);
+
+    typeof { name:'Joe', age:18}
+    console.log('Joe');
+    console.log(18);
+
+    typeof myHat
+    console.log('myHat');
+
+   // STEP 11
+   var greetings = "Hello, Luisa";
+   var statement = " welcome to the Javascript class";
+   alert (greetings + statement);
+
+   // STEP 12
+   alert ('Hello Zak Ruvalcaba, welcome to the JavaScript class!');
+
+    // STEP 13
+     var name = "Hello, Luisa";
+     var javaScript = " welcome to the Javascript class";
+      alert (name + javaScript);
+
+    // STEP 14
+     var name = "Hello, Luisa";
+     var javaScript = " \nWelcome to the Javascript class";
+     alert (name + javaScript);
+
+    // STEP 15
+   var name = prompt("What is your name");
+   console.log (name);
+
+   // STEP 16
+   var javaScript = prompt("What class are you taking?");
+   window.prompt(javaScript);
+
+   // STEP 17
+   var x = 10;
+   var y = 20;
+   console.log(x + y);
+
+    // STEP 18
+   var x = 20 + 20;
+   console.log(x);
+
+    // STEP 19
+   var x = 20;
+   console.log(x * 5);
+
+    // STEP 20
+   var x = 20;
+   var y = x % 3;
+   var z = y/1;
+   console.log(z);
+ 
+    // STEP 21
+   var x = 5;
+   console.log (x != 6);
+
+    // STEP 22
+
+    var x = 20;
+    var y = 5;
+    console.log (x < y);
+
+   // STEP 23
+
+   var widget = {widget};
+   console.log (typeof {widget});
+
+  // STEP 24
+   
+  var widget = ["widget"];
+  console.log (widget instanceof Object);
+
+ // STEP 25
+   
+  var widget = ["widget"];
+  console.log (widget instanceof Number);
+
+ 
+   
+    
